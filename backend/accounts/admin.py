d5aa501d10e62039e2d97f1e560b7c8d77959af2
MozdzoM <mozdzom@MozdzoMs-MacBook.local>
2026-05-09 13:39:27 +0200

    Fix: Accounts app & admin config

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+from .models import User, UserSettings
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    list_display = ("username", "email", "is_active", "is_staff")
+    add_fieldsets = DjangoUserAdmin.add_fieldsets + ((None, {"fields": ("email",)}),)
+
+
+@admin.register(UserSettings)
+class UserSettingsAdmin(admin.ModelAdmin):
+    list_display = ("user", "language", "theme", "font_size")
+    search_fields = ("user__username", "user__email")
